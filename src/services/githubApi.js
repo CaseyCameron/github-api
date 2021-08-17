cc9d62a7b0e9051e6de20c79735b7b8ce1675d41
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-08-17 16:38:31 -0700

    added github fetches in js

---
+++ src/services/githubApi.js
@@ -0,0 +1,11 @@
+export const fetchGitHeads = async (username) => {
+  const user =  await fetch(`https://api.github.com/users/${username}`);
+  const json = await user.json();
+  return json;
+};
+
+export const fetchGitRepos = async (username) => {
+  const user = await fetch(`https://api.github.com/${username}/repos`);
+  const json = await user.json();
+  return json;
+};
