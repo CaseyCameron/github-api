7e856aa4b13690f25638cb02e3cdae7305e18e7d
Katherine Tam <katherinemtam@gmail.com>
2021-08-18 10:42:11 -0700

    add user avatar image and Link to repos to userProfile

--- src/components/users/UserInfo.jsx
+++ src/components/users/UserInfo.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useUser } from '../../state/UserProvider';
 
 const UserInfo = () => {
@@ -6,11 +7,13 @@ const UserInfo = () => {
   return (
     <>
       <h2>rendering userInfo</h2>
+      <img src={user.avatar_url} alt={user.login} />
       <p>Login: {user.login}</p>
       <p>Name: {user.name}</p>
       <p>Followers: {user.followers}</p>
       <p>Following: {user.following}</p>
       <p>Profile: {user.url}</p>
+      <Link to={`/${user.login}/repos`}>Check out {user.login}'s Repos</Link>
     </>
   );
 };
