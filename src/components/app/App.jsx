567fdce4dfc7c5f84a5f657d353b90c10edd9ec2
Katherine Tam <katherinemtam@gmail.com>
2021-08-18 10:41:20 -0700

    add repo route

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Header from '../header/Header';
+import RepoList from '../repos/RepoList';
 import UserInfo from '../users/UserInfo';
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={UserInfo} />
+        <Route exact path="/:user/repos" component={RepoList} />
       </Switch>
     </>
   );
